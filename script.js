e55a80c6b27de0d400d14904d3305759e818fa20
Onimae <manufelipearias@gmail.com>
2023-01-09 21:53:14 -0500

    adding shopping cart side window desktop and mobile, add animations opening and closing menus

--- script.js
+++ script.js
@@ -1,22 +1,46 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+
 const menuHamIcon = document.querySelector(".menu")
 const mobileMenu = document.querySelector(".mobile-menu")
 
-
+const menuShopCarIcon = document.querySelector(".navbar-shopping-cart")
+const aside = document.querySelector(".product-detail")
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuShopCarIcon.addEventListener('click',toggleSoppingCart)
 
 
 function toggleDesktopMenu() {
-  desktopMenu.classList.toggle('inactive');
+
+    if(!aside.classList.contains("hide-menu-cart")){
+        aside.classList.add("hide-menu-cart")
+    }
+
+    desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
-    mobileMenu.classList.toggle('hide-menu');
-}
 
+    if(!aside.classList.contains("hide-menu-cart")){
+        aside.classList.add("hide-menu-cart")
+    }
+
+    mobileMenu.classList.toggle('hide-menu');     
+};
+
+function toggleSoppingCart() {    
+
+    if(!mobileMenu.classList.contains("hide-menu")){
+        mobileMenu.classList.add("hide-menu")
+    };
+
+    if(!desktopMenu.classList.contains("inactive")){
+        desktopMenu.classList.add("inactive")
+    }
+    aside.classList.toggle('hide-menu-cart');
+};
 
 
 
